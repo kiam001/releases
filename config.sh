6ce19221b93859f2b11d8586c30621ffd7eaf403
JarlPenguin <jarlpenguin@gmail.com>
2022-11-30 11:41:10 +0100

    PL2: init Project-Elixir

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="PL2"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="Elixir"
+export ROM_DIR="$WORKSPACE/../Project-Elixir-Tiramisu"
+export ROM_VERSION="Tiramisu"
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/releases/Project-Elixir-Tiramisu-PL2/manifest.xml"
+export manifest_url="https://github.com/Project-Elixir/manifest"
+export rom_vendor_name="aosp"
+export branch="Tiramisu"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="true"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="kiam001/releases"
 
-export timezone="UTC"
+export timezone="Europe/Berlin"

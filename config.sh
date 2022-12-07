9485d8550ae444c278989e54ed37d8142f86e5fc
kiam001 <blablatupfen@gmail.com>
2022-12-07 14:49:22 +0000

    Rice init

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="PL2"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="RiceDroid"
+export ROM_DIR="$WORKSPACE/../RiceDroid-thirteen"
+export ROM_VERSION="thirteen"
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/releases/RiceDroid-thirteen-PL2/manifest.xml"
+export manifest_url="https://github.com/RiceDroid/android"
+export rom_vendor_name="lineage"
+export branch="thirteen"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="true"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="kiam001/releases"
 
-export timezone="UTC"
+export timezone="Europe/Berlin"

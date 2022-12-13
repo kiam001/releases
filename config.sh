78bcf14ad6d8b60b15201664ee9485ea1cd1d761
kiam001 <blablatupfen@gmail.com>
2022-12-13 15:12:51 +0000

    init

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="lancelot"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="PixelOS-AOSP"
+export ROM_DIR="$WORKSPACE/../PixelOS-AOSP-thirteen"
+export ROM_VERSION="thirteen"
+export local_manifest_url="https://gitlab.com/R9Lab/Manifest/-/raw/PixelOS-12.1/local_manifest.xml"
+export manifest_url="https://github.com/PixelOS-AOSP/manifest"
+export rom_vendor_name="aosp"
+export branch="thirteen"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="true"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="kiam001/releases"
 
-export timezone="UTC"
+export timezone="Europe/Berlin"

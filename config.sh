f64db41cfcb5cbea702e5da542e1a42b5f6bab43
kiam001 <blablatupfen@gmail.com>
2022-12-18 13:49:17 +0000

    init

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="beryllium"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export ROM="PixelExperience"
+export ROM_DIR="$WORKSPACE/../PixelExperience-twelve-plus"
+export ROM_VERSION="twelve-plus"
+export local_manifest_url="https://raw.githubusercontent.com/RadeonPS/local_manifest/beryllium_pe12/local_manifest.xml"
+export manifest_url="https://github.com/PixelExperience/manifest"
+export rom_vendor_name="aosp"
+export branch="twelve-plus"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="true"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="kiam001/releases"
 
-export timezone="UTC"
+export timezone="Europe/Berlin"

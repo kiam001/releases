c8f8b57b242b02acb81341d8695f6bac9f9c93ad
Mizushudesuyo: 転生 <sugoidekai488@gmail.com>
2023-04-23 21:17:05 +0300

    releases: Initial support for lavender
    Signed-off-by: Mizushudesuyo: 転生 <sugoidekai488@gmail.com>

--- config.sh
+++ config.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-export GITHUB_USER="kiam001"
-export GITHUB_EMAIL="blablatupfen@gmail.com"
+export GITHUB_USER="Mizushudesuyo: 転生"
+export GITHUB_EMAIL="sugoidekai488@gmail.com"
 
-export device="vayu"
+export device="lavender"
 
-export ROM="crdroidandroid"
-export ROM_DIR="$WORKSPACE/../crdroidandroid-13.0"
-export ROM_VERSION="13.0"
-export local_manifest_url="https://raw.githubusercontent.com/kiam001/releases/crdroidandroid-13.0-vayu/manifest.xml"
-export manifest_url="https://github.com/crdroidandroid/android"
-export rom_vendor_name="lineage"
-export branch="13.0"
-export bacon="bacon"
-export buildtype="userdebug"
+export ROM="DerpFest-AOSP"
+export ROM_DIR="$WORKSPACE/../DerpFest-AOSP"
+export ROM_VERSION="13"
+export local_manifest_url="https://raw.githubusercontent.com//mizuswork/local_manifests/local_manifest.xml"
+export manifest_url="https://github.com/DerpFest-AOSP/manifest"
+export rom_vendor_name="derp"
+export branch="13"
+export bacon="derp"
+export buildtype="user"
 export clean="installclean"
-export generate_incremental="true"
 export upload_recovery="true"
 
 export ccache="true"
